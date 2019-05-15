ad044edad29cb9ebabcd29b16eb731d1938ed0cb
somerandomguyontheinternut <50674179+somerandomguyontheinternut@users.noreply.github.com>
2019-05-16 02:27:09 +0300

    Add files via upload

--- pokerp/gamemode/modules/menus/cl_f4menu.lua
+++ pokerp/gamemode/modules/menus/cl_f4menu.lua
@@ -15,6 +15,9 @@ end
 function PRM_MApi.Derma(DPanel,Parent,Size,Pos,TextIfAny)
 ]]--
 function PokeRP.CustomizeF4Frame()
+	PokeRP.F4Frame:MakePopup()
+	PokeRP.F4Frame:SetSize(500,500)
+	PokeRP.F4Frame:Center()
 	PRP_MApi.Derma("DButton",PokeRP.F4Frame,PRP_MApi.Vector2D(50,50),PRP_MApi.Vector2D(220,225),"Trainer",nil,function() net.Start("PokeRP_SetPoke") net.WriteInt(-1,32) net.SendToServer() end)
 	PRP_MApi.Derma("DButton",PokeRP.F4Frame,PRP_MApi.Vector2D(50,50),PRP_MApi.Vector2D(280,225),"Pokemon",nil,function() PokeRP.PokeMenu() end)
 end
