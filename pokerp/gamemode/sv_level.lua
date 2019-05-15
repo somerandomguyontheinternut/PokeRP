4ec4f6a1705fde3161ca0be6f7b1e29f5df8d170
somerandomguyontheinternut <50674179+somerandomguyontheinternut@users.noreply.github.com>
2019-05-15 22:27:18 +0300

    Delete sv_level.lua

--- pokerp/gamemode/sv_level.lua
+++
@@ -1,9 +0,0 @@
-local plymeta = FindMetaTable( "Player" )
-
-function plymeta:SetLevel(amount)
-    MySQLite.query([[UPDATE pokerp_player SET level = ]] .. amount .. [[ WHERE uid = ]] .. self:SteamID64())
-end
-
-function plymeta:AddLevel(Level)
-    self:SetMoney(self:GetNWInt("level") + Level)
-end
