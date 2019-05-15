b22f602e1d3e2d417981d435589437b515ef462f
somerandomguyontheinternut <50674179+somerandomguyontheinternut@users.noreply.github.com>
2019-05-15 22:31:21 +0300

    Add files via upload

---
+++ pokerp/gamemode/modules/menus/cl_f4menu.lua
@@ -0,0 +1,18 @@
+function PokeRP.F4Menu()
+	if(IsValid(PokeRP.F4Frame)) then return end
+	PokeRP.F4Frame = vgui.Create("DFrame")
+end
+
+
+--[[
+To Make Custom F4 Menus Override This Function
+function PokeRP.CustomizeF4Frame
+	-- ur shit
+end
+
+
+function PRM_MApi.Derma(DPanel,Parent,Size,Pos,TextIfAny)
+]]--
+function PokeRP.CustomizeF4Frame()
+	PRP_MApi.Derma("DButton",PokeRP.F4Frame,PRP_MApi.2DVector(50,50),PRP_MApi.2DVector(50,50),"Hey",nil,function() print("ha") end)
+end
