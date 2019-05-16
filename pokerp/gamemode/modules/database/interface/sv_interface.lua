25acbe46abb79d8c08df79e5cfda5f7eac262b30
somerandomguyontheinternut <50674179+somerandomguyontheinternut@users.noreply.github.com>
2019-05-16 05:24:41 +0300

    Update sv_interface.lua

--- pokerp/gamemode/modules/database/interface/sv_interface.lua
+++ pokerp/gamemode/modules/database/interface/sv_interface.lua
@@ -2,11 +2,11 @@ local PlayerMeta = FindMetaTable("Player")
 
 
 function PlayerMeta:SetMoney(amnt)
-		MySQLite.query([[UPDATE pokerp_player SET money = ]] .. amnt .. [[ WHERE uid = ]] .. self:SteamID64())
-		self:SetNWInt("PokeRP_Money",amnt)
+	MySQLite.query([[UPDATE pokerp_player SET money = ]] .. amnt .. [[ WHERE uid = ]] .. self:SteamID64())
+	self:SetNWInt("PokeRP_Money",amnt)
 end
 
 function PlayerMeta:SetLevel(amnt)
-		MySQLite.query([[UPDATE pokerp_player SET level = ]] .. amnt .. [[ WHERE uid = ]] .. self:SteamID64())
-		self:SetNWInt("PokeRP_Level",amnt)
+	MySQLite.query([[UPDATE pokerp_player SET level = ]] .. amnt .. [[ WHERE uid = ]] .. self:SteamID64())
+	self:SetNWInt("PokeRP_Level",amnt)
 end
