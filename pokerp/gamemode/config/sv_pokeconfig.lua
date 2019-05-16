54e9e849d77facebfd3c7d8cf3ec9db87b99683b
somerandomguyontheinternut <50674179+somerandomguyontheinternut@users.noreply.github.com>
2019-05-16 16:34:21 +0300

    Delete sv_pokeconfig.lua

--- pokerp/gamemode/config/sv_pokeconfig.lua
+++
@@ -1,9 +0,0 @@
-PokeRP.Pokemons = {
-    {name = "Charizard", health = "56", movesets = {"move_firefang"}, model = "models/yunpolmodels/pokemon/gen 1/charmander.mdl" },
-    {name = "Bulbasaur", health = "45", movesets = {"move_megadrain"}, model = "models/yunpolmodels/pokemon/gen 1/bulbasaur.mdl" },
-    {name = "Pikachu", health = "50", movesets = {"move_thunderbolt"}, model = "models/yunpolmodels/pokemon/gen 1/pikachu_male.mdl"}
-}
--- Crowbar is just an Example
-PokeRP.TrainerWeapons = {
-"weapon_crowbar"
-}
